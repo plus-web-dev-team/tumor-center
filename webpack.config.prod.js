44f87c2a5a9819879e9ae2d6611ea35b31b790d2
plus-agc <yamauchi-t@plus-agc.com>
2025-05-16 15:50:29 +0900

    modified

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,9 +1,21 @@
-const path = require('path');
+const path = require('node:path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const glob = require('glob');
+const fs = require('node:fs');
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+
+const sitemapPaths = fs.readdirSync(__dirname)
+  .filter((file) => path.extname(file) === '.html' && file !== '404.html')
+  .map((file) => {
+    const stats = fs.statSync(path.join(__dirname, file));
+    return {
+      path: file === 'index.html' ? '/' : `/${path.basename(file, '.html')}`,
+      lastmod: stats.mtime.toISOString().split('T')[0],
+    };
+  });
 
 module.exports = merge(common, {
   mode: 'production',
@@ -27,5 +39,6 @@ module.exports = merge(common, {
         { from: 'site.webmanifest', to: 'site.webmanifest' },
       ],
     }),
+    new SitemapPlugin({ base: 'https://example.com', paths: sitemapPaths }),
   ],
 });
